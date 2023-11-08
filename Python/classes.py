bebaaaa40da139ce01d911f5a38248eccab7dcc8
mohamedlagnibi <ithri.ojnna.985@gmail.com>
2023-11-08 14:06:19 -0800

    Classes

---
+++ Python/classes.py
@@ -0,0 +1,11 @@
+class Point():
+    """Create a class"""
+
+    def __init__(self, input1, input2):
+        """Initialize the attribute."""
+        self.x = input1
+        self.y = input2
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
