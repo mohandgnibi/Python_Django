8be3b5678817dab0f3b12baf1d0eb3c6226bce9f
mohamedlagnibi <ithri.ojnna.985@gmail.com>
2023-11-09 21:46:52 +0100

    Lambda funciton

---
+++ Python/lambda1.py
@@ -0,0 +1,10 @@
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Darco", "house": "Slytherin"}
+]
+
+
+people.sort(key=lambda person: person['name'])
+
+print(people)
