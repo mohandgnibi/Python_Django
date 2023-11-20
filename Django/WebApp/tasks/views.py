3d3e3798820127603fd0efcaa3667710818ddd33
mohamedlagnibi <ithri.ojnna.985@gmail.com>
2023-11-20 23:04:22 +0100

    Django

--- Django/WebApp/tasks/views.py
+++ Django/WebApp/tasks/views.py
@@ -5,7 +5,7 @@ tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+    
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
