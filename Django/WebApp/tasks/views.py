44cf652bde17781441b5a5f982c20207b7d3d0fe
mohamedlagnibi <ithri.ojnna.985@gmail.com>
2023-11-20 23:13:56 +0100

    Django

--- Django/WebApp/tasks/views.py
+++ Django/WebApp/tasks/views.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -18,6 +20,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return(request, "tasks/add.html", {
                 "form": form
