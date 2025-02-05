dfa9ab39167900c680894beadf6723ddca0e6197
vostro <vostro@example.com>
2025-02-05 21:41:34 +0100

    Lecture5: JavaScript
    Using Local Storage to store information that we can when a user returns to the site.
    Using localStorage.getItem(key) function
    Using localStorage.setItem(key, value)

--- JavaScript/counter.js
+++ JavaScript/counter.js
@@ -1,12 +1,24 @@
-let counter = 0;
+// Check if there is already a value in local storage.
+if (!localStorage.getItem('counter')) {
+
+    // If not, set the value of counter to 0 in local storage.
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    // Retrieve the current value of the counter from local storage.
+    let counter = localStorage.getItem('counter');
+
+    // Update the counter.
     counter++;
     document.querySelector('h1').innerHTML = counter;
+
+    // Store the updated counter in local storage.
+    localStorage.setItem('counter', counter);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Set heading to the value of the counter in local storage.
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
-
-    setInterval(count, 1000);
 });
